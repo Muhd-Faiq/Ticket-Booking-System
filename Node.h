3a68207563c61fe9e61596209cb2c052cbf1a06c
Muhammad Faiq Bin Roslan <mfaiqroslan@gmail.com>
2020-07-18 08:25:34 +0800

    Create Node.h

---
+++ Node.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#ifndef NODE_H
+#define NODE_H
+
+using namespace std;
+
+class Node
+{
+	public:
+		string data;	//data at the node
+		Node* next;		//pointer that point to next node
+		
+};
+
+#endif
